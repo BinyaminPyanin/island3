c17d16acec9ceda5f14d32025c32c175a3e84e27
BINYAMIN PYANIN <binyamin.pyanin@onespan.com>
2021-02-12 14:28:22 -0500

    Added my project

---
+++ db/src/main/resources/db/migration/V3__populate_initial_data.sql
@@ -0,0 +1,47 @@
+--USER_TYPE
+insert into public.USER_TYPE(TYPE,DESCRIPTION) values ('ADMIN','admin user');
+insert into public.USER_TYPE(TYPE,DESCRIPTION) values ('GUEST','guest user');
+insert into public.USER_TYPE(TYPE,DESCRIPTION) values ('REGISTERED','registered user');
+
+--STATUS
+insert into public.STATUS(CODE,DESCRIPTION) values ('AVAILABLE','Spot is available');
+insert into public.STATUS(CODE,DESCRIPTION) values ('RESERVED','Spot is reserved');
+insert into public.STATUS(CODE,DESCRIPTION) values ('CANCELED','Spot is canceled');
+
+--SPOT
+insert into public.SPOT(NAME,DESCRIPTION) values ('Retriever labrador','Retriever spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('German Shepherd Dogs	spot','German Shepherd Dogs spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Retrievers (Golden)','Retrievers (Golden)  spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('French Bulldogs','French Bulldogs spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Poodles','Poodles spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Beagles','Beagles spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Rottweilers','Rottweilers spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Pembroke Welsh Corgis','Pembroke Welsh Corgis spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Yorkshire Terriers','Yorkshire Terriers spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Australian Shepherds	spot','Australian Shepherds spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Boxers','Boxers spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Siberian Huskies	spot','Siberian Huskies spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Doberman Pinschers','Doberman Pinschers spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Shih Tzu	spot','Shih Tzu spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Boston Terriers','Boston Terriers spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Havanese	spot','Havanese spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Bernese Mountain','Bernese Mountain spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Pomeranians','Pomeranians spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Shetland Sheepdogs','Shetland Sheepdogs spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Brittanys','Brittanys spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Spaniels (English Springer)','Spaniels (English Springer) spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Spaniels (Cocker)','Spaniels (Cocker) spot spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Miniature American Shepherds	spot','Miniature American Shepherds spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Cane Corso','Cane Corso spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Pugs	spot','Pugs spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Mastiffs	spot','Mastiffs spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Border Collies','Border Collies spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Vizslas','Vizslas spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Chihuahuas','Chihuahuas spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Maltese','Maltese spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Basset Hounds','Basset Hounds spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Collies','Collies spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Weimaraners','Weimaraners spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Newfoundlands','Newfoundlands spot');
+insert into public.SPOT(NAME,DESCRIPTION) values ('Belgian Malinois','Belgian Malinois spot');
+
---
+++ island-application/src/main/java/com/upgrade/island3/Island3Application.java
@@ -0,0 +1,13 @@
+package com.upgrade.island3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Island3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Island3Application.class, args);
+	}
+
+}
